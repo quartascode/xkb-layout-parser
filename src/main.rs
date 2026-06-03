31eadd2e2e954c65142116e4976d5b2d5b9b54c6
quartascode <brenoposavac@proton.me>
2026-06-03 20:07:57 -0300

    LINHAS

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
+use std::io::Write;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
+use std::path;
 
+fn main() {
+    let file = File::open("/home/breno/.config/xkb/symbols/xkb-test").unwrap();
+
+    let reader = BufReader::new(file);
+
+    for line in reader.lines() {
+        let line = line.unwrap();
+        let line = line.trim();
+
+        print!("{}\n", line);
+    }
 }
